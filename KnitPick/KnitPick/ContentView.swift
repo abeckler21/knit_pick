7a21197790034d0ef0dd0434bfc96a8fdb871e29
abby beckler <102563768+abeckler21@users.noreply.github.com>
2026-02-26 11:20:21 -0600

    adding knit pick...

---
+++ KnitPick/KnitPick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KnitPick
+//
+//  Created by Abigail Beckler on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KnitPick/KnitPick/KnitPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnitPickApp.swift
+//  KnitPick
+//
+//  Created by Abigail Beckler on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct KnitPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
