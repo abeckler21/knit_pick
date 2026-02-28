8e41c4088264441f4d124a6f9ad9aa945bf39a83
abby beckler <102563768+abeckler21@users.noreply.github.com>
2026-02-28 12:43:50 -0600

    adding the tools page with some bad sample examples

--- KnitPick/KnitPick/ContentView.swift
+++ KnitPick/KnitPick/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ToolView()
+                .tabItem { Label("Tools", systemImage: "wrench.and.screwdriver") }
         }
-        .padding()
     }
 }
 
